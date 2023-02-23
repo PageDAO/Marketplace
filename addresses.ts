e8ac1f0b3fcb46cdf82741370d0d19cc00e86dde
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-02-22 23:58:45 -0600

    updating

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,2 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0x5dbc7b840baa9dabcbe9d2492e45d7244b54a2a0';
+export const marketplaceContractAddress = '0x159BCB789c77007dd047d2b0E8Beb4e3f225395f';
