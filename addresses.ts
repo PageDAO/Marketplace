4c84c7fa08f3dec91fb856059e3020a41f0d266c
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-02-23 23:38:22 -0600

    updated to SPTest Thirdweb marketplace contract address

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,2 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0x159BCB789c77007dd047d2b0E8Beb4e3f225395f';
+export const marketplaceContractAddress = '0x636324e1FE59E1e71ad766B5e06b06FF2A0606Af';
